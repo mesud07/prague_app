f445615c3a072575694664bb1f0d100f33ab5dcb
mesud07 <56825677+mesud07@users.noreply.github.com>
2021-08-19 13:13:04 +0300

    Home page tasarlandı fakat düzeltmeler yapılacak

--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:prague_app/model/slidermodel.dart';
+import 'package:prague_app/widgets/homepage_button.dart';
+
 class HomePage extends StatefulWidget {
-
-
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -14,11 +16,93 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         backgroundColor: Colors.white,
         toolbarHeight: 40,
-        title: Center(child: Text("PRAGUE CoolPass",style: GoogleFonts.ubuntu(color: Colors.black,fontWeight: FontWeight.bold,letterSpacing: 1),),),
-
+        elevation: 30,
+        title: Center(
+          child: Text(
+            "PRAGUE CoolPass",
+            style: GoogleFonts.ubuntu(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1),
+          ),
+        ),
       ),
       body: Container(
-        color: Colors.black,
+        color: Colors.white30,
+        child: Column(
+          children: [
+            Expanded(
+
+                flex: 1,
+                child: Container(
+                  padding: EdgeInsets.only(top: 5,left: 5,right: 5),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                        Homepage_Button("MY COOLPASS", Icons.smartphone),
+                          Homepage_Button("ATTRACTIONS", Icons.attractions),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Homepage_Button("TIPS & ALERRTS", Icons.warning_outlined),
+                          Homepage_Button("BUY COOLPASS", Icons.credit_card),
+                        ],
+                      ),
+
+                    ],
+                  ),
+                )
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 12),
+
+              alignment: Alignment.centerLeft,
+              child: Text("MOST POPULAR",style: GoogleFonts.rubik(fontSize: 20),),
+            ),
+            Expanded(
+              flex: 1,
+             child: ListView.builder(
+                 physics: NeverScrollableScrollPhysics(),
+                 itemCount: pageItemList.length,
+                 itemBuilder: (context,index){
+               return Container(
+                 height: 200,
+                 padding: EdgeInsets.all(10),
+                 margin: EdgeInsets.all(10),
+                 decoration: BoxDecoration(
+                   image: DecorationImage(
+                     image: AssetImage(pageItemList[index].urlname),
+                     fit: BoxFit.cover,
+                   ),
+                 ),
+                 child: Column(
+                   mainAxisAlignment: MainAxisAlignment.end,
+                   children: [
+
+                     Container(
+                         padding: EdgeInsets.only(left: 10),
+                         alignment: Alignment.centerLeft,
+                         child: Text(
+                           pageItemList[index].title,
+                           style:
+                           GoogleFonts.ubuntu(fontSize: 26, color: Colors.white),
+                         )),
+
+
+
+
+
+                   ],
+                 ),
+               );
+             }),
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/homepage_button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+class Homepage_Button extends StatelessWidget {
+String butontext;
+IconData icon;
+
+Homepage_Button(this.butontext,this.icon);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      height: 120,
+      width: 170,
+      child: InkWell(
+        splashColor: Colors.yellowAccent,
+        highlightColor: Colors.black,
+        onTap: () async {},
+        child: Material(
+          elevation: 4,
+          shadowColor: Colors.grey,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                padding: EdgeInsets.only(top: 5),
+                child: Icon(
+                icon,
+                color: Colors.deepOrange,
+                size: 70.0,
+                semanticLabel: 'Text to announce in accessibility modes',
+              ),),
+              Container(
+
+                  padding: EdgeInsets.only(left: 10, right: 10,bottom: 5),
+                  child: Text(butontext,)),
+
+            ]
+          ),
+        ),
+      ),
+    );
+  }
+}
